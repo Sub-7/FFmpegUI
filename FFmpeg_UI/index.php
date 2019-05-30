a7d838f2d875627e9dd8e5e2996c9c06795726ad
Sub7 <30667941+Sub-7@users.noreply.github.com>
2019-05-30 16:50:08 +0200

    Update index.php

--- FFmpeg_UI/index.php
+++ FFmpeg_UI/index.php
@@ -82,47 +82,42 @@ if ($video_codec == '-c:v libx264' or $video_codec == '-c:v libx265' or $video_c
            $newfile = 'ffmpeg_encoding_history.txt';
           
          if (!copy($file, $newfile)) 
-		  {
+	   {
             echo "copy $file ERROR...\n";
-          }
+           }
           unlink('progress.txt');
-        
-        if (isset($start))
-         {
-           $encode_time = round(microtime(true) * 1) - $start;
-           $hours = (int)($encode_time/60/60);
-           $minutes = (int)($encode_time/60)-$hours*60;
-           $seconds = (int)$encode_time-$hours*60*60-$minutes*60;
-	   echo "</br>Encoding time " . $hours .":". $minutes .":". $seconds . "</br> ... done,</br> select a new file or press 'Reload'</br>";
-   	}
+         if (isset($start))
+           {
+            $encode_time = round(microtime(true) * 1) - $start;
+            $hours = (int)($encode_time/60/60);
+            $minutes = (int)($encode_time/60)-$hours*60;
+            $seconds = (int)$encode_time-$hours*60*60-$minutes*60;
+	    echo "</br>Encoding time " . $hours .":". $minutes .":". $seconds . "</br> ... done,</br> select a new file or press 'Reload'</br>";
+   	   }
 ////////////////////////////////////// get media info using ffprobe
-        if(isset($_SESSION["file"]))
-         {
-			if (file_exists($path.$_SESSION["file"])) 
-		   {
-	       $video_info = $path.$_SESSION["file"];
-	       exec("ffprobe -i $video_info -v quiet -print_format xml -show_format -show_streams -hide_banner > temp_file", $output, $res);
-	       $duration = exec("ffmpeg -i $video_info 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//");
-	       $encoder = exec("ffmpeg -i $video_info 2>&1 | grep encoder");
-               $info = new SimpleXMLElement(file_get_contents("temp_file"));
-	       $size = filesize($video_info);
-               $GB = round(($size / 1073741824), 3); // bytes to GB
-	       $mime_type = mime_content_type($video_info);
-	       $audio_track= exec("ffmpeg -i $video_info 2>&1 | grep Stream >track");
-		   			
+          if(isset($_SESSION["file"]))
+           {
+	    if (file_exists($path.$_SESSION["file"])) 
+	   {
+	    $video_info = $path.$_SESSION["file"];
+	    exec("ffprobe -i $video_info -v quiet -print_format xml -show_format -show_streams -hide_banner > temp_file", $output, $res);
+            $duration = exec("ffmpeg -i $video_info 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//");
+            $encoder = exec("ffmpeg -i $video_info 2>&1 | grep encoder");
+            $info = new SimpleXMLElement(file_get_contents("temp_file"));
+	    $size = filesize($video_info);
+            $GB = round(($size / 1073741824), 3); // bytes to GB
+	    $mime_type = mime_content_type($video_info);
+	    $audio_track= exec("ffmpeg -i $video_info 2>&1 | grep Stream >track");
 ?>
-
 <!--////////////////////////////////////// here is form with FFmpeg options-->
-
-<form name="file" action='index.php' method='post'>
-    <table  border="0">
-		
-	   <tr > 
-		<th>Video codec</th> <th>Video bitrate</th> <th>Scale</th>  <th>Format</th>      
-	   </tr>
-      <td >
-	    <select name="video_codec"> 
-          <option selected value="-c:v h264_vaapi">H.264 VAAPI</option>  <!--// note 'selected' in this line, this value is preselected in the form-->
+        <form name="file" action='index.php' method='post'>
+        <table  border="0">
+	<tr > 
+	<th>Video codec</th> <th>Video bitrate</th> <th>Scale</th>  <th>Format</th>      
+	</tr>
+        <td >
+	<select name="video_codec"> 
+        <option selected value="-c:v h264_vaapi">H.264 VAAPI</option>  <!--// note 'selected' in this line, this value is preselected in the form-->
 		  <option value="-c:v h264_nvenc">H.264 CUDA</option>  
 		  <option value="-c:v hevc_nvenc">HEVC CUDA</option>
 		  <option value="-c:v hevc_vaapi">HEVC VAAPI</option>	
@@ -130,39 +125,35 @@ if ($video_codec == '-c:v libx264' or $video_codec == '-c:v libx265' or $video_c
 		  <option value="-c:v vp9_vaapi">VP9 VAAPI</option>
 		  <option value="-c:v mpeg2_vaapi">MPEG-2 VAAPI</option>
 		  <option value="-c:v libx264">x.264</option>
-	      <option value="-c:v libx265">x.265 HEVC</option>
+	          <option value="-c:v libx265">x.265 HEVC</option>
 		  <option value="-c:v vp8_v4l2m2m">VP8</option>
 		  <option value="-c:v libvpx-vp9">VP9</option>
-	      <option value="-c:v mpeg2video">MPEG-2</option>
+	          <option value="-c:v mpeg2video">MPEG-2</option>
 		  <option value="-c:v mpeg4">MPEG-4</option>
 		  <option value="-c:v wmv1">WNV 7</option>
-	      <option value="-c:v wmv2">WMV 8</option>
-         </select>
-      </td>
-	  <td >
-	    <select name="video_bitrate"> 
-         <option value="-b:v 1M -maxrate 1M -bufsize 1M">1.000 kb/s</option>
-	     <option value="-b:v 1.5M -maxrate 1.5M -bufsize 1.5M">1.500 kb/s</option>
-         <option value="-b:v 2M -maxrate 2M -bufsize 2M">2.000 kb/s</option>
-         <option selected value="-b:v 3M -maxrate 3M -bufsize 3M">3.000 kb/s</option>  
-	     <option value="-b:v 4M -maxrate 4M -bufsize 4M">4.000 kb/s</option>
-	     <option value="-b:v 5M -maxrate 5M -bufsize 5M">5.000 kb/s</option>
-	     <option value="-b:v 6M -maxrate 6M -bufsize 6M">6.000 kb/s</option>
-	     <option value="-b:v 7M -maxrate 7M -bufsize 7M">7.000 kb/s</option>
-	     <option value="-b:v 8M -maxrate 8M -bufsize 8M">8.000 kb/s</option>
-	     <option value="-b:v 9M -maxrate 9M -bufsize 9M">9.000 kb/s</option>
-	     <option value="-b:v 10M -maxrate 10M -bufsize 10M">10.000 kb/s</option>
-	     <option value="-b:v 15M -maxrate 15M -bufsize 15M">15.000 kb/s</option>
-	     <option value="-b:v 20M -maxrate 20M -bufsize 20M">20.000 kb/s</option>
-	     <option value="-b:v 30M -maxrate 30M -bufsize 30M">30.000 kb/s</option>
-	     <option value="-b:v 40M -maxrate 40M -bufsize 40M">40.000 kb/s</option>
-         <option value="-b:v 50M -maxrate 50M -bufsize 50M">50.000 kb/s</option>
-	    </select>
-      </td>
-     <td >
-	    <select name="scale"> 
+	          <option value="-c:v wmv2">WMV 8</option>
+         </select></td>
+	 <td ><select name="video_bitrate"> 
+                  <option value="-b:v 1M -maxrate 1M -bufsize 1M">1.000 kb/s</option>
+	          <option value="-b:v 1.5M -maxrate 1.5M -bufsize 1.5M">1.500 kb/s</option>
+                  <option value="-b:v 2M -maxrate 2M -bufsize 2M">2.000 kb/s</option>
+                  <option selected value="-b:v 3M -maxrate 3M -bufsize 3M">3.000 kb/s</option>  
+	          <option value="-b:v 4M -maxrate 4M -bufsize 4M">4.000 kb/s</option>
+	          <option value="-b:v 5M -maxrate 5M -bufsize 5M">5.000 kb/s</option>
+	          <option value="-b:v 6M -maxrate 6M -bufsize 6M">6.000 kb/s</option>
+	          <option value="-b:v 7M -maxrate 7M -bufsize 7M">7.000 kb/s</option>
+	          <option value="-b:v 8M -maxrate 8M -bufsize 8M">8.000 kb/s</option>
+      	          <option value="-b:v 9M -maxrate 9M -bufsize 9M">9.000 kb/s</option>
+	          <option value="-b:v 10M -maxrate 10M -bufsize 10M">10.000 kb/s</option>
+	          <option value="-b:v 15M -maxrate 15M -bufsize 15M">15.000 kb/s</option>
+	          <option value="-b:v 20M -maxrate 20M -bufsize 20M">20.000 kb/s</option>
+	          <option value="-b:v 30M -maxrate 30M -bufsize 30M">30.000 kb/s</option>
+	          <option value="-b:v 40M -maxrate 40M -bufsize 40M">40.000 kb/s</option>
+                  <option value="-b:v 50M -maxrate 50M -bufsize 50M">50.000 kb/s</option>
+	   </select></td>
+           <td ><select name="scale"> 
                    <option value="-vf scale_vaapi=w=-2:h=576">576p</option>
-          <option selected value="-vf scale_vaapi=h=-2:w=1280">720p</option>   
+                   <option selected value="-vf scale_vaapi=h=-2:w=1280">720p</option>   
 	           <option value="-vf scale_vaapi=h=-2:w=1920">1080p</option>
 	           <option value="-vf scale_vaapi=h=-2:w=2048">2K UHD</option>
                    <option value="-vf scale_vaapi=h=-2:w=4096">4K UHD</option>
@@ -171,56 +162,43 @@ if ($video_codec == '-c:v libx264' or $video_codec == '-c:v libx265' or $video_c
                    <option value="-vf scale_vaapi=w=-2:h=240">240p</option>
 	           <option value="-vf scale_vaapi=w=-2:h=360">360p</option>
                    <option value="-vf scale_vaapi=w=-2:h=480">480p</option>
-        </select>
-      </td>
-      <td >	  
-	    <select name="format"> 
-          <option value="-f matroska">MKV</option>
-          <option value="-f flv">FLV</option>
-	      <option value="-f mp4">MP4</option>
-		  <option value="-f avi">AVI</option>
-		  <option value="-f wmv">WMV</option>
-        </select>
-      </td>
+            </select></td>
+            <td ><select name="format"> 
+                   <option value="-f matroska">MKV</option>
+                   <option value="-f flv">FLV</option>
+	           <option value="-f mp4">MP4</option>
+	  	   <option value="-f avi">AVI</option>
+		   <option value="-f wmv">WMV</option>
+             </select></td>
 <!--0:1#############################################################################################################################-->
-		<tr >
-		<th >Audio Track 0:1</th>  <th>Audio codec</th> <th>Audio bitrate</th>     
-	   </tr>
-		
-	   <td >
-	      <select  name="map_0_1">
-			<option value="">nothing</option>
-            <option selected value="-map 0:1">0:1</option> <!--////// note 'selected' in this line, this value is preselected in the form-->
-		</select>
-      </td>
-      <td >	  
-		<select name="map_0_1_codec">
-		  <option value="">nothing</option> 
-          <option selected value="-codec:a:0 copy">Copy</option>  <!--////// note 'selected' in this line, this value is preselected in the form-->
-		  <option value="-codec:a:0 dca -strict -2">DTS</option>
-		  <option value="-codec:a:0 truehd -strict -2">True HD</option>
-          <option value="-codec:a:0 libfdk_aac">DD</option>
-	      <option value="-codec:a:0 eac3">DD+</option>
-	      <option value="-codec:a:0 mp2">MP2</option>
-	      <option value="-codec:a:0 aac">AAC LC</option>
-	      <option value="-codec:a:0 libfdk_aac">HE-AAC</option>
-	      <option value="-codec:a:0 libvorbis">Vorbis</option>
-	      <option value="-codec:a:0 libmp3lame">MP3</option>
-	      <option value="-codec:a:0 libopus -strict -2">Opus</option>
-	    </select>
-      </td>	
-	  <td >	  
-		<select name="map_0_1_bitrate">
-		  <option selected value="">nothing</option> <!--////// note 'selected' in this line, this value is preselected in the form-->
-		  <option value="">Copy</option>  
-          <option value="-b:a:0 1536k">1536 kb/s</option>
-		  <option value="-b:a:0 768k">768 kb/s</option>
-		  <option value="-b:a:0 384k">384 kb/s</option> 
-		  <option value="-b:a:0 192k">192 kb/s</option>
-		  <option value="-b:a:0 128k">128 kb/s</option>
-		  
-	    </select>
-	 </td>
+		<tr ><th >Audio Track 0:1</th>  <th>Audio codec</th> <th>Audio bitrate</th></tr>
+             <td ><select  name="map_0_1">
+		    <option value="">nothing</option>
+                    <option selected value="-map 0:1">0:1</option> <!--////// note 'selected' in this line, this value is preselected in the form-->
+	     </select></td>
+             <td ><select name="map_0_1_codec">
+		    <option value="">nothing</option> 
+                    <option selected value="-codec:a:0 copy">Copy</option>  <!--////// note 'selected' in this line, this value is preselected in the form-->
+		    <option value="-codec:a:0 dca -strict -2">DTS</option>
+		    <option value="-codec:a:0 truehd -strict -2">True HD</option>
+                    <option value="-codec:a:0 libfdk_aac">DD</option>
+	            <option value="-codec:a:0 eac3">DD+</option>
+	            <option value="-codec:a:0 mp2">MP2</option>
+	            <option value="-codec:a:0 aac">AAC LC</option>
+	            <option value="-codec:a:0 libfdk_aac">HE-AAC</option>
+	            <option value="-codec:a:0 libvorbis">Vorbis</option>
+	            <option value="-codec:a:0 libmp3lame">MP3</option>
+	            <option value="-codec:a:0 libopus -strict -2">Opus</option>
+	       </select></td>	
+	       <td ><select name="map_0_1_bitrate">
+		    <option selected value="">nothing</option> <!--////// note 'selected' in this line, this value is preselected in the form-->
+		    <option value="">Copy</option>  
+                    <option value="-b:a:0 1536k">1536 kb/s</option>
+		    <option value="-b:a:0 768k">768 kb/s</option>
+		    <option value="-b:a:0 384k">384 kb/s</option> 
+		    <option value="-b:a:0 192k">192 kb/s</option>
+		    <option value="-b:a:0 128k">128 kb/s</option>
+		</select></td>
 <!--0:2#############################################################################################################################-->
 				<tr >
 		<th >Audio Track 0:2</th>  <th>Audio codec</th> <th>Audio bitrate</th>     
