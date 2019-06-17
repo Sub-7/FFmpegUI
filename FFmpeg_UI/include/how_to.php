b2e69de4e02252811f52beba2546b339cae9e5e3
Sub7 <30667941+Sub-7@users.noreply.github.com>
2019-06-17 20:23:29 +0200

    Delete how_to.php

--- FFmpeg_UI/include/how_to.php
+++
@@ -1,4 +0,0 @@
-<?php
-$compile_ffmpeg = file_get_contents('./include/compile_ffmpeg.txt');
-echo "<details><summary>FFmpeg Compilation Guide</summary><pre>$compile_ffmpeg<br></pre></details><hr>";
-?>
