289fe712da7de64315b5527b5e9613b7be451d8d
Sub7 <30667941+Sub-7@users.noreply.github.com>
2021-04-03 16:57:23 +0200

    Update ffmpeg.php

--- FFmpeg_UI/ffmpeg.php
+++ FFmpeg_UI/ffmpeg.php
@@ -46,7 +46,7 @@
         $output_file_format = substr($path. 'encoded_' . $_POST['file'] , 0, - 4);
         $loglevel          = "-loglevel info";
 	$vaapi = " -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -hwaccel_output_format vaapi";
-	$nvidia_gpu = "-hwaccel cuvid -c:v h264_cuvid";
+	$nvidia_gpu = "-hwaccel cuda -hwaccel_output_format cuda -c:v h264_cuvid";
     
 	
 	///VAAPI
@@ -62,9 +62,8 @@
 	{
 	$scale_nv=$_POST["scale"];
 	$scale_npp = str_replace('_vaapi', '_npp', $scale_nv);
-	echo $scale_npp."<hr>";
-	$ffmpeg_line = "ffmpeg $loglevel $nvidia_gpu -y -i $input_file -map 0:0 $map_0_1 $map_0_2 $map_0_3 $map_0_4 $map_0_5 $map_0_6 $map_0_7 $scale $video_bitrate $video_codec $map_0_1_codec $map_0_1_bitrate $map_0_2_codec $map_0_2_bitrate $map_0_3_codec $map_0_3_bitrate $map_0_4_codec $map_0_4_bitrate $map_0_5_codec $map_0_5_bitrate $map_0_6_codec $map_0_6_bitrate $map_0_7_subtitle $output_file_format.$file_end $format 2> progress.txt";
-	//echo $ffmpeg_line; ///shows what ffmpeg is doing, here with CUDA
+	$ffmpeg_line = "ffmpeg $loglevel $nvidia_gpu -y -i $input_file -map 0:0 $map_0_1 $map_0_2 $map_0_3 $map_0_4 $map_0_5 $map_0_6 $map_0_7 $scale_npp $video_bitrate $video_codec $map_0_1_codec $map_0_1_bitrate $map_0_2_codec $map_0_2_bitrate $map_0_3_codec $map_0_3_bitrate $map_0_4_codec $map_0_4_bitrate $map_0_5_codec $map_0_5_bitrate $map_0_6_codec $map_0_6_bitrate $map_0_7_subtitle $output_file_format.$file_end $format 2> progress.txt";
+	echo $ffmpeg_line; ///shows what ffmpeg is doing, here with CUDA
 	$ffmpeg_encode = shell_exec("$ffmpeg_line");
 	}    
 	
@@ -555,4 +554,3 @@
     }
     }
 	?>
-		
